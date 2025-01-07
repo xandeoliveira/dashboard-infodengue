ccd599c224f058218d1b64e110c18f8e4d86e239
Alexandre Lima <oliveiralexandrelima@hotmail.com>
2025-01-07 14:52:54 -0300

    Added conversion functions and an csv test.

--- pre_processamento/meses.py
+++ pre_processamento/meses.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
+# FUNÇÕES DE API
 def request_city_info(geocode, ws, we, ys, ye):
   URL = "https://info.dengue.mat.br/api/alertcity"
   params = (
@@ -30,6 +31,8 @@ def get_city_info(city, interval):
                           'casprov_est_max','casconf'], inplace=True)
   return city_info
 
+
+# FUNÇÕES DE "CONVERSÃO"
 def get_main_indexes(df):
   # data_iniSE no formato YYYY-MM-DD, convertendo para MM-YYYY
   df = df[['data_iniSE']]
@@ -42,7 +45,7 @@ def get_main_indexes(df):
 
   return dates
 
-def get_monthly_cases(df, main_indexes, unique_indexes):
+def get_monthly_cases(df, unique_indexes):
   # Somando os casos para um mesmo mês
   df = df[['data_iniSE','casos']]
   month_cases = list()
@@ -61,41 +64,81 @@ def get_monthly_dataframe(data_array):
 
   return df_monthly_cases
 
-# def get_monthly_temp(df, main_indexes, unique_indexes):
-#     # Somando os casos para um mesmo mês
-#   df = df[['data_iniSE','tempmin','tempmed','tempmax']]
-#   tempmin = list()
-#   tempmed = list()
-#   tempmax = list()
+def get_monthly_temp_and_hum(df, unique_indexes):
+  # Tirando as médias para um mesmo mês
+  df = df[['data_iniSE','tempmin','tempmed','tempmax','umidmin','umidmed','umidmax']]
+  tempmin = list()
+  tempmed = list()
+  tempmax = list()
+  umidmin = list()
+  umidmed = list()
+  umidmax = list()
 
-#   for month in unique_indexes:
-#     weeks = df[df['data_iniSE'] == month]
-#     tempmin.append(weeks['tempmin'].sum() / len(weeks['tempmin']))
-#     tempmed.append(weeks['tempmed'].sum() / len(weeks['tempmed']))
-#     tempmax.append(weeks['tempmax'].sum() / len(weeks['tempmax']))
+  for month in unique_indexes:
+    weeks = df[df['data_iniSE'] == month]
 
-#   return tempmin, tempmed, tempmax
+    tempmin.append(weeks['tempmin'].sum() / len(weeks['tempmin']))
+    tempmed.append(weeks['tempmed'].sum() / len(weeks['tempmed']))
+    tempmax.append(weeks['tempmax'].sum() / len(weeks['tempmax']))
 
+    umidmin.append(weeks['umidmin'].sum() / len(weeks['umidmin']))
+    umidmed.append(weeks['umidmed'].sum() / len(weeks['umidmed']))
+    umidmax.append(weeks['umidmax'].sum() / len(weeks['umidmax']))
+
+  monthly_temp_and_hum = {
+    'tempmin': tempmin, 'tempmed': tempmed, 'tempmax': tempmax,
+    'umidmin': umidmin, 'umidmed': umidmed, 'umidmax': umidmax
+    }
+  
+  return monthly_temp_and_hum
+
+def get_level_pop_and_accum(df, unique_indexes):
+  # Obtendo a população e os casos anuais
+  df = df[['data_iniSE','nivel','pop','notif_accum_year']]
+  levels = list()
+  pop = list()
+  accum = list()
+
+  for month in unique_indexes:
+    weeks = df[df['data_iniSE'] == month]
+    levels.append(weeks['nivel'].max())
+    pop.append(weeks['pop'].max())
+    accum.append(weeks['notif_accum_year'].max())
+  
+    level_pop_and_accum = { 'nível': levels, 'população': pop, 'casos_acumulados_ano': accum }
+
+  return level_pop_and_accum
+
+# PRÉ-PROCESSAMENTO PARA O INTERVALO DE EXEMPLO
 interval = (1, 2024, 53, 2024)
 city = "Acarape"
 
 df_main = get_city_info(city, interval)
 main_indexes = get_main_indexes(df_main)
+
+df_main.loc[:, 'data_iniSE'] = main_indexes
 unique_indexes = np.unique(main_indexes)
 
-years = np.reshape( np.array([date[:4] for date in unique_indexes]), -1 )
-months = np.reshape( np.array([date[5:7] for date in unique_indexes]), -1 )
-monthly_data = {
+indexes_data = {
     'Index': unique_indexes,
-    'Ano': years,
-    'Mês': months,
-    'Casos': get_monthly_cases(df_main, main_indexes, unique_indexes)
+    'ano': np.reshape( np.array([date[:4] for date in unique_indexes]), -1 ),
+    'mês': np.reshape( np.array([date[5:7] for date in unique_indexes]), -1 ),
+}
+monthly_temp_and_hum = get_monthly_temp_and_hum(df_main, unique_indexes)
+level_pop_and_accum = get_level_pop_and_accum(df_main, unique_indexes)
+
+monthly_data = {
+    **indexes_data,
+    'casos': get_monthly_cases(df_main, unique_indexes),
+    **monthly_temp_and_hum,
+    **level_pop_and_accum
 }
 
+# Dados pré-processados
 df_preprocessed = get_monthly_dataframe(monthly_data)
 
-# # Salvando o dataframe
-# separador = '\\' if os.name == 'nt' else '/'
-# caminho = f'{os.getcwd()}{separador}{city}_monthly.csv'
+# SALVANDO O DATAFRAME
+separador = '\\' if os.name == 'nt' else '/'
+caminho = f'{os.getcwd()}{separador}{city}_monthly.csv'
 
-# df_preprocessed.to_csv(caminho, index=False)
+df_preprocessed.to_csv(caminho, index=False)
